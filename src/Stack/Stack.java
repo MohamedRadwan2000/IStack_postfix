a8d045e7260613b27558533ff2f550236b248f4f
MohamedRadwan2000 <60818439+MohamedRadwan2000@users.noreply.github.com>
2020-04-03 23:33:16 +0200

    Update Stack.java

--- src/Stack/Stack.java
+++ src/Stack/Stack.java
@@ -10,12 +10,19 @@ public class Stack implements IStack{
     }
       private StackNode head;
       private int size=0;
+    /***
+     * this function tests if this stack if empty or not
+     * @return true if the stack is empty and false if its not empty
+     */
     public boolean isEmpty(){
         if(head==null){
             return true;
         }
         else {return false;}
     }
+    /***
+     * this function push the value you want to push into the stack
+     */
     public void push(Object data){
         if(this.head==null){
             StackNode n=new StackNode(data);
@@ -29,6 +36,11 @@ public class Stack implements IStack{
             n.next=temp;
             this.size++;}
     }
+    /***
+     * this function pop from the stack
+     * it throws exception if the stack is already empty
+     * @return the poped value
+     */
     public Object pop(){
         if(head==null){
             throw new NullPointerException("STACK IS EMPTY");
@@ -38,6 +50,11 @@ public class Stack implements IStack{
         this.size--;
         return data;}
     }
+    /***
+     * this function peek from the stack
+     * it throws exception if the stack is already empty
+     * @return the peeked value
+     */
     public Object peek(){
         if(head==null){
             throw new NullPointerException("STACK IS EMPTY");
@@ -45,8 +62,15 @@ public class Stack implements IStack{
         else {Object data=head.data;
             return data;}
     }
+    /***
+     * this function return the size of the stack
+     * @return the size of the stack
+     */
     public int size(){return this.size;}
-
+    /***
+     * this function turn the stack into array to make performing tests on it more easier
+     * @return array with the values on the stack
+     */
     public Object[]ToArray(){
         Object[]y=new Object[this.size()];
         for(int i=0;i<y.length;i++){
